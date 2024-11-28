c987588abb717021ed956cb729e89acbca4e6abc
Fabrice Harel-Canada <fabricehc@pluslab-a6000.cs.ucla.edu>
2024-11-27 23:16:58 -0800

    changing paths to the grammars

--- xg/demos/c_demo.py
+++ xg/demos/c_demo.py
@@ -4,17 +4,20 @@ from xg.generator import GrammarBasedGenerator
 
 if __name__ == "__main__":
     # Configuration
-    MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
+    MODEL_ID = "Qwen/Qwen2.5-Coder-7B-Instruct" # "meta-llama/Llama-3.2-3B-Instruct"
     CACHE_DIR = "/data2/.shared_models"
-    EBNF_PATH = "/data2/fabricehc/cdt/grammars/c.gbnf"
+    EBNF_PATH = "./grammars/c.gbnf"
+    NUM_TESTS = 3
 
     # Initialize the generator
     generator = GrammarBasedGenerator(MODEL_ID, CACHE_DIR, EBNF_PATH)
 
     # Prompts for generation
-    SYSTEM_PROMPT = "You are a helpful assistant."
-    USER_PROMPT = "Write a short C implementation of a Fibonacci function. Only respond with C code. Code:"
+    SYSTEM_PROMPT = "You are a helpful coding assistant."
+    USER_PROMPT = "Write a unintuitive C implementation of the Fibonacci function."
 
     # Generate text
-    generated_code = generator.generate(SYSTEM_PROMPT, USER_PROMPT)
-    print(generated_code)
+    for test in range(NUM_TESTS):
+        print(f"Test #{test+1}:")
+        generated_code = generator.generate(SYSTEM_PROMPT, USER_PROMPT)
+        print(generated_code)
--- xg/demos/json_demo.py
+++ xg/demos/json_demo.py
@@ -6,15 +6,18 @@ if __name__ == "__main__":
     # Configuration
     MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
     CACHE_DIR = "/data2/.shared_models"
-    EBNF_PATH = "/data2/fabricehc/cdt/grammars/json.gbnf"
+    EBNF_PATH = "./grammars/json.gbnf"
+    NUM_TESTS = 3
 
     # Initialize the generator
     generator = GrammarBasedGenerator(MODEL_ID, CACHE_DIR, EBNF_PATH)
 
     # Prompts for generation
-    SYSTEM_PROMPT = "You are a helpful assistant."
+    SYSTEM_PROMPT = "You are a helpful coding assistant."
     USER_PROMPT = "Introduce yourself in JSON."
 
     # Generate text
-    generated_code = generator.generate(SYSTEM_PROMPT, USER_PROMPT)
-    print(generated_code)
+    for test in range(NUM_TESTS):
+        print(f"Test #{test+1}:")
+        generated_code = generator.generate(SYSTEM_PROMPT, USER_PROMPT)
+        print(generated_code)
